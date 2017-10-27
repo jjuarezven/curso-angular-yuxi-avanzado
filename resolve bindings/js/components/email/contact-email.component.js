22ef1808c6271addaf79f1353dcc05e01a08b432
JJuarez <jose.juarez@yuxipacific.com>
2017-10-27 10:29:17 -0500

    routing continuacion

---
+++ resolve bindings/js/components/email/contact-email.component.js
@@ -0,0 +1,15 @@
+var contactEmail = {
+  template: `
+        <div>Contact via email: josejuarez78@gmail.com</div>
+    `
+};
+
+angular
+  .module("contact")
+  .component("contactEmail", contactEmail)
+  .config(function($stateProvider) {
+    $stateProvider.state("contact.email", {
+      url: "/email",
+      component: "contactEmail"
+    });
+  });
