e1e79af94ae008cec81b1f9aeb366839145d5646
JJuarez <jose.juarez@yuxipacific.com>
2017-11-01 15:06:06 -0500

    fin routing

---
+++ ui router directives/js/components/contact/phone/contact-phone.component.js
@@ -0,0 +1,20 @@
+var contactPhone = {
+  template: `
+        <div>Contact via phone: +584125749932</div>
+    `
+};
+
+angular
+  .module("contact")
+  .component("contactPhone", contactPhone)
+  .config(function($stateProvider) {
+    $stateProvider.state("contact.phone", {
+      url: "^/phone",
+      component: "contactPhone",
+      views: {
+        "details@contact": {
+          component: "contactPhone"
+        }
+      }
+    });
+  });
