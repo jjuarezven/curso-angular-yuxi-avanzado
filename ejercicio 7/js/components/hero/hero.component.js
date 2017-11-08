d7353e6b2bf33eee4e1ff3ec9106b3ee9085d438
JJuarez <jose.juarez@yuxipacific.com>
2017-11-08 13:37:44 -0500

    ejercicio 7 - 5

--- ejercicio 7/js/components/hero/hero.component.js
+++ ejercicio 7/js/components/hero/hero.component.js
@@ -1,28 +1,42 @@
-var hero = {
-	template: `
-		<div class="contact">
-			<div class="contact-methods">
-				<div ui-view="methods"></div>
-			</div>
-			<div class="contact-details" ui-view="details"></div>
+	var hero = {
+		template: `
+		<div class="contact contact-methods contact-details">
+			<ul>
+				<li ng-repeat="hero in $ctrl.heroes">
+					<div class="col-xs-18 col-sm-6 col-md-3">
+						<div class="thumbnail">
+							<a href="">
+								<img ng-src="{{hero.company}}" alt="">
+								<div class="caption">
+									<h4>{{hero.name}}</h4>
+									<p>{{hero.info}}</p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</li>
+			</ul>
 		</div>
-	`
-};
-
-angular
-	.module('hero')
-	.component('hero', hero)
-	.config(function ($stateProvider) {
-		$stateProvider
-			.state('hero', {
-				url: '/hero',
-				views: {
-					'@': {
-						component: 'hero'
-					},
-					'methods@hero': {
-						component: 'heroMethods'
-					}
-				}
-			});
-	});
+		  `,
+		controller: function() {
+		  var vm = this;
+		  vm.heroes = [
+			{ company: "images/dc logo.jpg", name: "Superman", info: "abc123" },
+			{ company: "images/dc logo.jpg", name: "Batman", info: "abc123" },
+			{ company: "images/marvel logo.png", name: "Thor", info: "abc123" },
+			{ company: "images/marvel logo.png", name: "Hulk", info: "abc123" }
+		  ];
+		}
+	  };
+	  
+	  angular
+	  .module('hero')
+	  .component('hero', hero)
+	  .config(function ($stateProvider) {
+		  $stateProvider
+			  .state('hero', {
+				  url: '/hero',
+				  component: 'hero'
+			  });
+	  });;
+	  
